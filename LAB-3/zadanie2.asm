1ac55d1491cb68d040b87e3f19dc05b9faa139ba
Bartas058 <kacper.prusak@wp.pl>
2023-04-24 18:42:44 +0200

    LAB-3

---
+++ LAB-3/zadanie2.asm
@@ -0,0 +1,19 @@
+org 100h
+
+MOV AH, 09h
+MOV DX, string
+INT 21h
+
+MOV AH, 02h
+MOV DL, 0Ah
+INT 21h
+
+MOV [string + 3], byte '$'
+MOV AH, 09h
+MOV DX, string
+INT 21h
+
+MOV AH, 00h
+INT 21h
+
+string db "GTS Macan Porsche"
